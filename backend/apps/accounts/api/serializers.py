a4f30e6b1d56d2245ae925876a2e98fb06399f81
Abhindev Vijayan <abhindevvijayan18@gmail.com>
2026-07-23 12:37:04 +0530

    Implement JWT authentication

--- backend/apps/accounts/api/serializers.py
+++ backend/apps/accounts/api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from apps.accounts.services.auth_service import create_user
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
@@ -20,3 +22,30 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return create_user(**validated_data)
+    
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            raise serializers.ValidationError(
+                "Invalid email or password."
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "user": user,
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        }
--- backend/apps/accounts/api/views.py
+++ backend/apps/accounts/api/views.py
@@ -1,7 +1,27 @@
 from rest_framework import generics
 
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer , LoginSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
+    
+    
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        data = serializer.validated_data
+
+        return Response(
+            {
+                "access": data["access"],
+                "refresh": data["refresh"],
+            },
+            status=status.HTTP_200_OK,
+        )
