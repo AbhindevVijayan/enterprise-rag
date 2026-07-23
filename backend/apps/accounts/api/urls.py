47622dc0ebc674e2ef8eacbb9c6ee7a7a99242e8
Abhindev Vijayan <abhindevvijayan18@gmail.com>
2026-07-23 12:49:39 +0530

    Complete JWT authentication module

--- backend/apps/accounts/api/urls.py
+++ backend/apps/accounts/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views import UserRegistrationView
 from .views import LoginView
+from .views import MeView
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("me/", MeView.as_view(), name="me"),
 ]
 
